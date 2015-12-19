bfcf09e1946b8b49f6598a85091fbaac7787af3c
Lila Muse <alatiel.ck@gmail.com>
2015-12-19 11:26:43 +0200

    add class point

---
+++ snake/snake/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Point
+{
+    public int x;
+    public int y;
+    public char symb;
+
+    public void Draw()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(symb);
+    }
+}
